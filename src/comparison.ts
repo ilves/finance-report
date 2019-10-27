dbdb78fba199a90069955723100adcca24bd2881
taavi.ilves <taavi.ilves@bigbank.ee>
2019-10-27 08:07:24 +0200

    Progress

--- src/comparison.ts
+++ src/comparison.ts
@@ -1,45 +1,74 @@
-import * as _ from 'lodash';
-import * as moment from 'moment';
+import * as _ from 'lodash'
+import * as moment from 'moment'
 
 export class Comparison {
-  data: Object[];
+  data: _.CollectionChain<Object>;
   opts: ComparisonOpts;
   report: Object;
-  fields: String[];
+  _groups: _.ObjectChain<_.Dictionary<Object[]>>
+  _value: {};
+  _fields: Object[];
 
   constructor (data: Object[], opts: ComparisonOpts) {
-    this.data = data;
-    this.opts = Object.assign({}, opts);
-    this.calculate();
-  }
-
-  calculate () {
-    this.fields = Object.keys(_.groupBy(this.data, this.opts.valueBy));
-    this.report = _.chain(this.data)
-      .groupBy(this.opts.groupBy)
-      .mapValues(values => _.groupBy(values, this.opts.valueBy))
-      .value();
-    this.report = this.fields.reduce((all, field) => {
-      all[field] = this.groups().map(group => this.opts.amountReducer(this.report[group][field] || []));
-      return all;
-    }, {});
+    this.data = _.chain(data)
+    this.opts = Object.assign({}, opts)
+    this.report = {}
+    this._groups = this.data.groupBy(this.opts.groupBy)
+    this._fields = []
+    this._value = this.calculateValue()
   }
 
   groups () {
-    return Object.keys(this.report).sort();
+    return this._groups.keys().sort().value()
   }
 
-  values () {
-    return Object.values(this.report)
+  value () {
+    return this._value
+  }
+
+  fields () {
+    return this._fields
+  }
+
+  calculateValue () {
+    const groupBy = (item) => {
+      const key = this.opts.valueBy(item)
+      const pos = _.findIndex(this._fields, (i) => _.isEqual(i, key))
+      const res = pos >= 0 ? pos : this._fields.push(key) - 1
+      return res
+    }
+
+    const res = this._groups
+      .mapValues(values => {
+        return _.chain(values).groupBy(groupBy).mapValues(val => val.reduce(this.opts.sumBy, {})).value()
+      })
+      .transform((result, value, key) => {
+        _.forEach(value, (item, k) => {
+          result[k] = result[k] || _.zipObject(this.groups(), _.map(this.groups(), () => this.opts.sumBy({}, {})))
+          result[k][key] = item
+        })
+      }, {})
+      .mapValues((values, key) => ({
+        values: Object.values(values),
+        field: this._fields[key]
+      }))
+      .values()
+      .value()
+
+    return res
+  }
+
+  total () {
+    return this._groups.mapValues(val => val.reduce(this.opts.sumBy, {})).value()
   }
 
   static timeseries (dateField: string, format: string = 'YYYY-MM') {
-    return (item) => moment(item[dateField]).format(format);
+    return (item) => moment(item[dateField]).format(format)
   }
 }
 
 export interface ComparisonOpts {
-  amountReducer: Function;
-  valueBy: Function | string;
+  sumBy: any;
+  valueBy: any | string;
   groupBy: Function;
 }
--- test/comparison.test.ts
+++ test/comparison.test.ts
@@ -1,16 +1,17 @@
+import * as _ from 'lodash'
 import {
   Comparison
-} from './../src/comparison';
+} from './../src/comparison'
 
 const txs = [
-  tx('2019-03-01', 'Intress', 15),
-  tx('2019-02-01', 'Intress', 25),
-  tx('2019-01-01', 'Intress', 35),
-  tx('2019-01-01', 'Põhiosa', 55),
-  tx('2019-02-01', 'Põhiosa', 65),
-  tx('2019-03-01', 'Põhiosa', 75),
-  tx('2019-02-01', 'Viivis',  12),
-  tx('2019-01-15', 'Intress', 10),
+  tx('2019-03-01', 'Intress', 15, 'stock'),
+  tx('2019-02-01', 'Intress', 25, 'stock'),
+  tx('2019-01-01', 'Intress', 35, 'stock'),
+  tx('2019-01-01', 'Põhiosa', 55, 'stock'),
+  tx('2019-02-01', 'Põhiosa', 65, 'stock'),
+  tx('2019-03-01', 'Põhiosa', 75, 'stock'),
+  tx('2019-02-01', 'Viivis', 12, 'stock'),
+  tx('2019-01-15', 'Intress', 10, 'bond')
 ]
 
 describe('Comparison', () => {
@@ -18,23 +19,97 @@ describe('Comparison', () => {
     it('should work', () => {
       const report = new Comparison(txs, {
         groupBy: Comparison.timeseries('date'),
-        valueBy: 'desc',
-        amountReducer: (items) => items.reduce((sum, cur) => sum + cur.value, 0)
-      });
+        valueBy: ({ desc }) => ({ desc }),
+        sumBy: (a, b) => {
+          a.value = (a.value || 0) + b.value
+          return a
+        }
+      })
       expect(report.groups()).toEqual([
-        "2019-01",
-        "2019-02",
-        "2019-03",
-      ]);
-      expect(report.fields).toEqual([
-        "Intress",
-        "Põhiosa",
-        "Viivis",
-      ]);
-    });
-  });
-});
+        '2019-01',
+        '2019-02',
+        '2019-03'
+      ])
+      expect(report.fields()).toEqual([
+        { desc: 'Intress' },
+        { desc: 'Põhiosa' },
+        { desc: 'Viivis' }
+      ])
+      expect(report.value()).toEqual([
+        {
+          values: [
+            {
+              value: 45
+            },
+            {
+              value: 25
+            },
+            {
+              value: 15
+            }
+          ],
+          field: { desc: 'Intress' }
+        },
+        {
+          values: [
+            {
+              value: 55
+            },
+            {
+              value: 65
+            },
+            {
+              value: 75
+            }
+          ],
+          field: { desc: 'Põhiosa' }
+        },
+        {
+          values: [
+            {
+              value: NaN
+            },
+            {
+              value: 12
+            },
+            {
+              value: NaN
+            }
+          ],
+          field: { desc: 'Viivis' }
+        }
+      ])
+      expect(report.total()).toEqual({
+        '2019-01': {
+          value: 100
+        },
+        '2019-02': {
+          value: 102
+        },
+        '2019-03': {
+          value: 90
+        }
+      })
+    })
+    it('should group', () => {
+      const report = new Comparison(txs, {
+        groupBy: Comparison.timeseries('date'),
+        valueBy: ({ group, desc }) => ({ group, desc }),
+        sumBy: (a, b) => {
+          a.value = (a.value || 0) + b.value
+          return a
+        }
+      })
+      expect(report.fields()).toEqual([
+        { desc: 'Intress', group: 'stock' },
+        { desc: 'Põhiosa', group: 'stock' },
+        { desc: 'Viivis', group: 'stock' },
+        { desc: 'Intress', group: 'bond' }
+      ])
+    })
+  })
+})
 
-function tx (date, desc, value) {
-  return { date, desc, value };
-};
+function tx (date, desc, value, group) {
+  return { date, desc, value, group }
+}
